fa54cf47474419598d3487d963c2ebfff3f8d064
Rahul makhija <rahulmkhj@gmail.com>
2017-09-21 19:32:25 +0530

    several modifications to AccountsController
    1. updated getAdd method to include available zones in the view for
    selection.
    2. after new account add, redirect to the newly created account details
    instead of list of accounts.

--- src/Http/Controllers/AccountsController.php
+++ src/Http/Controllers/AccountsController.php
@@ -6,13 +6,15 @@ namespace AccessManager\Accounts\Http\Controllers;
 use AccessManager\Accounts\Http\Requests\CustomerAccountRequest;
 use AccessManager\Accounts\Models\Account;
 use AccessManager\Base\Http\Controller\AdminBaseController;
+use AccessManager\Zones\Models\Zone;
+use Illuminate\Http\Request;
 
 /**
  *  This class takes care of all the operations related to customer accounts, which
  * includes creating, updating, deleting and showing the list of present
  * accounts in the database.
  * Class AccountsController
- * @package AccessManager\Accounts\Http\Controllers
+ * @package AccessManager\Accounts
  */
 class AccountsController extends AdminBaseController
 {
@@ -32,13 +34,53 @@ class AccountsController extends AdminBaseController
      */
     public function getAdd()
     {
-        return view('Accounts::add-edit');
+        $zones = Zone::pluck('name', 'id');
+        return view('Accounts::add', compact('zones'));
     }
 
+    /**
+     * @param CustomerAccountRequest $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function postAdd( CustomerAccountRequest $request )
     {
         try {
             Account::add($request);
+            return redirect()->route('account.details', [$request->username]);
+        }
+        catch (\Exception $e)
+        {
+            dd($e->getMessage());
+        }
+    }
+
+    /**
+     * @param $username
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function getEdit($username)
+    {
+        try {
+            $zones = Zone::pluck('name', 'id');
+            $account = Account::findForEdit($username);
+            return view("Accounts::edit", compact('account', 'zones'));
+        }
+        catch (\Exception $e)
+        {
+            dd($e->getMessage());
+        }
+    }
+
+    /**
+     * @param $username
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function postEdit($username, Request $request )
+    {
+        try {
+            $account = Account::where('username', $username)->firstOrFail();
+            $account->postEdit( $request );
             return redirect()->route('accounts.index');
         }
         catch (\Exception $e)
@@ -47,25 +89,20 @@ class AccountsController extends AdminBaseController
         }
     }
 
-    public function getEdit($username)
+    /**
+     * @param $username
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function postDelete($username)
     {
         try {
-            $account = Account::findForEdit($username);
-            return view("Account::add-edit", compact('account'));
+            $account = Account::where('username', $username)->firstOrFail();
+            $account->delete();
+            return redirect()->route('accounts.index');
         }
         catch (\Exception $e)
         {
             dd($e->getMessage());
         }
-    }
-
-    public function postEdit($username)
-    {
-
-    }
-
-    public function postDelete($username)
-    {
-
     }
 }
